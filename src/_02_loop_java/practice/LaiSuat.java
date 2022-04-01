5fee0d07fe5a8b889d78a7cd20fc475aed47a565
quoctrungdn102 <trungtranfpt12345@gmail.com>
2022-04-01 10:17:13 +0700

    m

--- src/_02_loop_java/practice/LaiSuat.java
+++ src/_02_loop_java/practice/LaiSuat.java
@@ -1,4 +1,24 @@
 package _02_loop_java.practice;
 
+import java.util.Scanner;
+
 public class LaiSuat {
+    public static void main(String[] args) {
+        double money = 1.0;
+        int month = 1;
+        double interestRate = 1.0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter investment amount: ");
+        money = input.nextDouble();
+        System.out.println("Enter number of months: ");
+        month = input.nextInt();
+        System.out.println("Enter annual interest rate in percentage: ");
+        interestRate = input.nextDouble();
+        double totalInterest = 0;
+        for(int i = 0; i < month; i++){
+            totalInterest += money * (interestRate/100)/12 * month;
+        }
+        System.out.println("Total of interest: " + totalInterest);
+
+    }
 }
--- src/_02_loop_java/practice/SoNguyenTo.java
+++ src/_02_loop_java/practice/SoNguyenTo.java
@@ -1,4 +1,21 @@
 package _02_loop_java.practice;
 
+import java.util.Scanner;
+
 public class SoNguyenTo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter your num ");
+        int count=0;
+        int number = scanner.nextInt();
+        for (int i = 1; i <=number ; i++) {
+            if(number%i==0){
+                count++;
+            }
+        }if(count==2){
+            System.out.println(number+"isPrime");
+        }else {
+            System.out.println(number+"is not prime" );
+        }
+    }
 }
