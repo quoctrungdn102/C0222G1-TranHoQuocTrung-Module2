afe36b9095d66a78bdebb6cc4d1c4e7b84810a56
quoctrungdn102 <trungtranfpt12345@gmail.com>
2022-04-05 21:52:57 +0700

    m

---
+++ src/_04_OOP/exercise/Fan.java
@@ -0,0 +1,4 @@
+package _04_OOP.exercise;
+
+public class Fan {
+}
--- src/_04_OOP/practice/Rectangle.java
+++ src/_04_OOP/practice/Rectangle.java
@@ -1,4 +1,35 @@
 package _04_OOP.practice;
 
+import java.util.Scanner;
+
 public class Rectangle {
+
+    double width,height;
+    public Rectangle(){
+    }
+    public Rectangle(double width,double height){
+        this.width = width;
+        this.height =height;
+    }
+    public double getArea(){
+        return this.width*this.height;
+    }
+    public double getPerimeter(){
+        return this.width+this.height;
+    }
+    public String display() {
+        return "Rectangle{" + "width=" + width + ", height=" + height + "}";
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter width");
+        double width = scanner.nextInt();
+        System.out.println("enter height");
+        double height = scanner.nextDouble();
+
+        Rectangle rectangle1 = new Rectangle(width,height);
+        System.out.println(rectangle1.display());
+        System.out.println( "dien tich"+ rectangle1.getArea());
+        System.out.println("chu vi "+rectangle1.getPerimeter());
+    }
 }
