b1f56c841662a424540478e0cc9df3732563448c
quoctrungdn102 <trungtranfpt12345@gmail.com>
2022-04-05 21:53:37 +0700

    m

--- src/_04_OOP/exercise/QuadraticEquation.java
+++ src/_04_OOP/exercise/QuadraticEquation.java
@@ -1,4 +1,75 @@
 package _04_OOP.exercise;
 
+import java.util.Scanner;
+
 public class QuadraticEquation {
+private double a,b,c;
+
+    public QuadraticEquation(){
+
+    }
+    public QuadraticEquation(double a, double b, double c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+    public double getDiscriminant(){
+        return (Math.pow(this.b,2) - 4*this.a*this.c);
+    }
+    public double  getRoot1(){
+        return ((-this.b)+Math.pow(this.b*this.b-4*this.a*this.c,0.5))/2*this.a;
+    }
+    public double  getRoot2(){
+        return ((-this.b)-Math.pow(this.b*this.b-4*this.a*this.c,0.5))/2*this.a;
+    }
+    public String display(){
+        return " The equation has no roots";
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("enter a");
+        double a = scanner.nextDouble();
+        System.out.print("enter b");
+        double b = scanner.nextDouble();
+        System.out.print("enter c");
+        double c = scanner.nextDouble();
+        QuadraticEquation quadraticEquation1 = new QuadraticEquation(a,b,c);
+
+      if(quadraticEquation1.getDiscriminant()>0){
+          System.out.println("x1 = "+quadraticEquation1.getRoot1());
+          System.out.println("x2 = "+quadraticEquation1.getRoot2());
+      }else if((quadraticEquation1.getDiscriminant()==0)){
+            System.out.println("x1=x2 = "+quadraticEquation1.getRoot1());
+      }else {
+          System.out.println(quadraticEquation1.display());
+      }
+    }
+
 }
+
--- src/_04_OOP/exercise/StopWatch.java
+++ src/_04_OOP/exercise/StopWatch.java
@@ -1,4 +1,80 @@
 package _04_OOP.exercise;
 
+import java.util.Arrays;
+import java.util.Date;
+
 public class StopWatch {
+
+    private long startTime, endTime;
+
+    public StopWatch() {
+        this.startTime = System.currentTimeMillis();
+//        this.endTime = System.currentTimeMillis();
+    }
+
+    public void setStartTime(long startTime) {
+        this.startTime = startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(long endTime) {
+        this.endTime = endTime;
+    }
+
+
+    public long start() {
+        return this.startTime;
+    }
+
+    public long sTop() {
+        return System.currentTimeMillis();
+    }
+
+    public long getElapsedTime() {
+        return (this.sTop() - this.startTime);
+    }
+
+
+
+
+    public static void main(String[] args) {
+        int[] arr = new int[100000];
+
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) Math.floor((Math.random() * 99.0) + 1.0);
+        }
+
+        StopWatch stopWatch = new StopWatch();
+
+
+//        long start = stopWatch.start();
+        stopWatch.start();
+        sort(arr);
+
+        stopWatch.sTop();
+//        long stop = stopWatch.sTop();
+//        System.out.println( stop -start);
+
+        System.out.println(stopWatch.getElapsedTime());
+    }
+
+    public static void sort(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                if (a[i] < a[j]) {
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+
+
+        }
+    }
 }
+//989
+//9
