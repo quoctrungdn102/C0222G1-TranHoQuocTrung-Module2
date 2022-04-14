b9b35874d0dc75563d54f260006e047063b3b741
quoctrungdn102 <trungtranfpt12345@gmail.com>
2022-04-14 23:07:19 +0700

    m

---
+++ src/_11_stack_queue/exercise/Binary.java
@@ -0,0 +1,21 @@
+package _11_stack_queue.exercise;
+
+import java.util.*;
+
+public class Binary {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Stack<Integer> stack = new Stack<>();
+        System.out.println("nhap 1 so thap phan ");
+        int nhap = scanner.nextInt();
+        while (nhap>0){
+          int binary = nhap%2;
+
+         nhap =  nhap/2;
+            stack.push(binary);
+        }
+        System.out.println(stack);
+    }
+
+    }
+
--- src/_11_stack_queue/exercise/TreeMap.java
+++ src/_11_stack_queue/exercise/TreeMap.java
@@ -1,10 +1,23 @@
 package _11_stack_queue.exercise;
 
+import java.security.Key;
+import java.time.temporal.ValueRange;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
+import java.util.Stack;
 
 public class TreeMap {
     public static void main(String[] args) {
-        Map
+        Scanner scanner = new Scanner(System.in);
+        String a = "tran ho quoc trung tran ";
+        System.out.println(a.toUpperCase());
+        String[] arr = a.toUpperCase().split(" ");
+
+
     }
 
-}
+    }
+
+
+
